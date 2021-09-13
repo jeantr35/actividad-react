d8315eb8ce3059c6b27f12d04da6b38b2713a759
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-13 14:25:27 -0500

    Creado el metodo para setear el nombre

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,54 +1,61 @@
 import React, { Fragment, useState } from "react";
 import { useForm } from "react-hook-form";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Formulario = () => {
 
-  const[user, setUser] = useState({
+    const[user, setUser] = useState({
         name:'',
         age:0,
         occupation:''
     });
 
-  const { register, handleSubmit, errors } = useForm();
+    const {register,handleSubmit, formState: { errors }} = useForm();
 
-  const [arrayUsers, setArrayUsers] = useState([])
-  
-  const onSubmit = (event) => {
-    setUser({
-        ...user,
-        [event.target.name] : event.target.value
-        
-      });
-  };
-  const handleInputChange = (event) =>{
-    setUser({
-      ...user,
-      [event.target.name] : event.target.value
-      
-    });
-    
-  }
+    const onsubmit = (data, e) =>{
+        setName(data.name)
+        e.target.reset()
+    }
 
-  
-  const [registered, setRegistered] = useState(false);
+    const setName = (name) => {
+        setUser({
+            ...user,
+            ["name"]: name
+        })
+    }
 
-  return (
+    const setAge = (age) => {
+        setUser({
+            ...user,
+            ["age"]: age
+        })
+    }
+
+   return(
+
     <Fragment>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <input onChange = {handleInputChange}
-            type="text"
-            name="name"
-            className="form-control my-2"
-            placeholder="Ingrese su nombre"
-          />
-          <button type="submit" className="btn btn-primary">
-               Enviar
-            </button>
-        </div>
-      </form>
+        <h2>Formulario</h2>
+        <form onSubmit={handleSubmit(onsubmit)}>
+            <input
+                className="form-control my-2"
+                name="name"
+                {...register("name", { 
+                    required:{
+                        value: true,
+                        message: 'name required' 
+                    }
+                })}   
+            />
+            <span className="text-danger text-small d-block mb-2">
+                {errors.name &&  errors.name.message}
+            </span>
+            <button className="btn btn-primary" >Send</button>
+        </form>
+
     </Fragment>
-  );
-};
+
+   )
+
+}
 
 export default Formulario;
